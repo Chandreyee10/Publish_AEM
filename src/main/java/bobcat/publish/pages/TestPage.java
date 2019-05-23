41107dfc1de1174e7180f00c98ae62da49dff518
Chandreyee10 <chandreyee.datta5600@gmail.com>
2019-05-23 18:56:50 +0530

    first commit

---
+++ src/main/java/bobcat/publish/pages/TestPage.java
@@ -0,0 +1,52 @@
+package bobcat.publish.pages;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import com.cognifide.qa.bb.aem.core.pages.AemAuthorPage;
+import com.cognifide.qa.bb.qualifier.PageObject;
+import com.cognifide.qa.bb.wait.BobcatWait;
+import com.google.inject.Inject;
+
+import com.cognifide.qa.bb.aem.page.PublishPage;
+
+@PageObject
+public class TestPage extends PublishPage {
+
+  @Inject
+  private BobcatWait bobcatWait;
+
+  private String title = "English";
+  private String url = "/content/we-retail/us/en.html";
+
+  public String getPageTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getContentPath() {
+	    return url;
+  }
+
+  public void setContentPath(String url) {
+    this.url = url;
+  }
+  
+  public boolean isDisplayed() {
+    return bobcatWait.isConditionMet(ExpectedConditions.titleIs(getPageTitle()));
+  }
+
+  public boolean isNotAvailable() {
+    return bobcatWait.isConditionMet(ExpectedConditions.titleContains("404 Resource"));
+  }
+  
+  public boolean isURLDisplayed() {
+	    return bobcatWait.isConditionMet(ExpectedConditions.urlToBe(getContentPath()));
+  }
+
+  public boolean isURLNotAvailable() {
+	    return bobcatWait.isConditionMet(ExpectedConditions.urlContains("404 Resource"));
+  }
+}
---
+++ src/test/java/bobcat/publish/login/AuthorizationTest.java
@@ -0,0 +1,48 @@
+package bobcat.publish.login;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import com.cognifide.qa.bb.aem.core.api.AemActions;
+import com.cognifide.qa.bb.api.actions.ActionException;
+import com.cognifide.qa.bb.api.actions.ActionsController;
+import com.cognifide.qa.bb.junit5.guice.Modules;
+import com.cognifide.qa.bb.modules.BobcatRunModule;
+import com.cognifide.qa.bb.page.BobcatPageFactory;
+import com.google.inject.Inject;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+
+import bobcat.publish.pages.TestPage;
+
+/**
+ * Test for presenting how to log in to the page.
+ */
+@Modules(BobcatRunModule.class)
+@Epic("AEM 6.4 Base Tests")
+@Feature("Login to AEM")
+public class AuthorizationTest {
+
+	@Inject
+	private ActionsController controller;
+
+	@Inject
+	private BobcatPageFactory bobcatPageFactory;
+	
+	@Inject
+	private TestPage testPage;
+
+	@Test
+	@Story("Login to AEM and open Test page")
+	@Description("Login to publish instance and open test page")
+	public void loginTest() throws ActionException {
+		//TestPage testpage = bobcatPageFactory.create("/content/we-retail/us/en.html", TestPage.class);
+		controller.execute(AemActions.LOG_IN);
+		testPage.open();
+		assertTrue(testPage.isDisplayed());
+	}
+
+}
